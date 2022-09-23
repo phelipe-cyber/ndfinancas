877ac2d7dc8d01ac5429f156382f7b8187179da1
phelipe-cyber <61544699+phelipe-cyber@users.noreply.github.com>
2022-09-23 11:34:12 -0300

    Update insert-chat.php

--- chat/php/insert-chat.php
+++ chat/php/insert-chat.php
@@ -5,13 +5,14 @@
         $outgoing_id = $_SESSION['unique_id'];
         $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
         $message = mysqli_real_escape_string($conn, $_POST['message']);
-        // $message = $message;
-
         if(!empty($message)){
-            // $sql = mysqli_query($conn, "INSERT INTO messages (incoming_msg_id,outgoing_msg_id,msg) VALUES ( {$incoming_id},{$outgoing_id}, DES_ENCRYPT('$message') )") or die() ;
-            $sql = mysqli_query($conn, "INSERT INTO messages (incoming_msg_id,outgoing_msg_id,msg) VALUES ( {$incoming_id},{$outgoing_id}, {$message} )") or die() ;
+            $sql = mysqli_query($conn, "INSERT INTO messages (incoming_msg_id, outgoing_msg_id, msg)
+                                        VALUES ({$incoming_id}, {$outgoing_id}, '{$message}')") or die();
         }
     }else{
         header("location: ../login.php");
-    }  
+    }
+
+
+    
 ?>

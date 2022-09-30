b8f6595187b5a40d24a0b57293c33eb6380f34ee
phelipe-cyber <61544699+phelipe-cyber@users.noreply.github.com>
2022-09-30 14:28:22 -0300

    Update salvar_edicao.php

--- salvar_edicao.php
+++ salvar_edicao.php
@@ -7,7 +7,7 @@ date_default_timezone_set('America/Recife');
  $usuario = $_SESSION['login'];
  $data_hora_salve = (date('Y-m-d_H:i:s'));
 
- $id_edit = $_POST['id_edit'];
+$id_edit = $_POST['id_edit'];
 $cpf = $_POST['cpf'];
 $cep = $_POST['cep'];
 $cnpj = $_POST['cnpj'];
@@ -36,6 +36,15 @@ $complemento_emp=$_POST['complemento_emp'];
 $referencia_emp=$_POST['referencia_emp'];
 $servico = $_POST['servico'];
 $socio = $_POST['servico'];
+
+$Sql_update_cliente = "UPDATE `clientes` SET `nome`='$nome',`sobrenome`='$sobrenome',`socio`='$razao_social',`cpf`='$cpf',
+`rg`='$rg',`tel`='$tel',`tel2`='$tel2',`atividade`='$atividade',`endereco`='$lougadouro',`cep`='$cep',
+`cnpj`='$cnpj',`numero`='$number',`bairro`='$bairro',`municipio`='$municipio',`uf`='$uf',
+`complemento`='$complemento',`referencia`='$referencia',`cep_emp`='$cep_emp',`lougadouro_emp`='$lougadouro_emp',
+`number_emp`='$number_emp',`municipio_emp`='$municipio_emp',`uf_emp`='$uf_emp',`bairro_emp`='$bairro_emp',
+`complemento_emp`='$complemento',`referencia_emp`='$referencia_emp' WHERE id = '$id_edit' ";
+
+$salvar = mysqli_query($conn, $Sql_update_cliente);
 
 $select_sql = ("SELECT * FROM `fotos_clientes` where id_cliente = '$id_edit' ");
 
